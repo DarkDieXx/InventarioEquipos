efabfdf8a439ba5bbab8c9f481122168cedad2b7
Hacybeyker <Hacybeyker@HACYBEYKERS>
2016-05-26 11:37:09 -0500

    Inplementacion del GestoODBC y ConexionSqlServer en la capa persistencia. La cadena de Conexion aun no esta definida.

---
+++ WebInvetarioEquipo/Modelo/c4_persistencia/GestorODBC.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.c4_persistencia
+{
+    public abstract class GestorODBC
+    {
+        protected SqlConnection conexion;
+        public abstract void abrirConexion();
+        public void cerrarConexion()
+        {
+            try
+            {
+                conexion.Close();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        public SqlCommand prepararSentencia(string sql)
+        {
+            SqlCommand sentencia;
+            return sentencia = new SqlCommand(sql, conexion);
+        }
+        public SqlDataReader ejecutarConsulta(string sql)
+        {
+            SqlDataReader resultado;
+            SqlCommand sentencia = prepararSentencia(sql);
+            resultado = sentencia.ExecuteReader();
+            return resultado;
+        }
+    }
+}
---
+++ WebInvetarioEquipo/Modelo/c4_persistencia/sqlserver/ConexionSqlServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.c4_persistencia.sqlserver
+{
+    public class ConexionSqlServer : GestorODBC
+    {
+        public override void abrirConexion()
+        {
+            try
+            {
+                string url = "";
+                conexion = new SqlConnection(url);
+                conexion.Open();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
